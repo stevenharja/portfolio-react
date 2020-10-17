4c0bd9692537172aeaab38456f9d5a26c68ebdf0
stevenharja <stevenharja@gmail.com>
2020-10-17 21:02:50 +1100

    Initial create react app commit

---
+++ src/container/App/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./App.scss";
+
+class App extends Component {
+  state = { projects: [{ name: "Project 1" }] };
+  render() {
+    return <div className="App">Test</div>;
+  }
+}
+
+export default App;
